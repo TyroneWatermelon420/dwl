867d31167c4e5f106201592f9c94e2efa88ae560
Tyrone Watermelon <erik.rosebrock@tutanota.com>
2024-05-06 17:29:27 +0200

    Fix

--- config.h
+++ config.h
@@ -149,7 +149,7 @@ static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TA
 
 /* commands */
 static const char *termcmd[]        = { "alacritty", NULL };
-static const char *menucmd[]        = { "wofi-run", NULL };
+static const char *menucmd[]        = { "wmenu-run", NULL };
 static const char *upvol[]          = { "/usr/bin/pactl",   "set-sink-volume", "0",      "+5%",      NULL };
 static const char *downvol[]        = { "/usr/bin/pactl",   "set-sink-volume", "0",      "-5%",      NULL };
 static const char *mutevol[]        = { "/usr/bin/pactl",   "set-sink-mute",   "0",      "toggle",   NULL };
